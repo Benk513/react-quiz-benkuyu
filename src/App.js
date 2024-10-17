46e45c59454259f2308f430960e234bc4e128c2b
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-10-17 02:09:24 +0100

    add App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
+import Header from './components/Header'
+import Error from './components/Error'
+import Loader from './components/Loader'
+import StartScreen from './components/StartScreen'
+import Question from './components/Question'
+import Progress from './components/Progress'
+import FinishScreen from './components/FinishScreen'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  questions: [],
+
+  // loading , ready , error , active, finished
+  status: 'loading',
+  currentQuestion: 0,
+  answer : null,
+  points :0,
+  highScore:0
 }
 
-export default App;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready'
+
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error'
+      }
+    case 'start':
+      return {
+        ...state,
+        status: 'active'
+      }
+    case 'newAnswer':
+      const question = state.questions.at(state.currentQuestion)
+      return {
+        ...state,
+        answer:action.payload,
+        points : action.payload === question.correctOption 
+        ? state.points + question.points 
+        : state.points
+      }
+    case 'nextQuestion':
+      return{
+        ...state,
+        currentQuestion : state.currentQuestion + 1,
+        answer : null
+      
+      }
+      case 'finish':
+        return{
+          ...state,
+          status : 'finished',
+          highScore : Math.max(state.highScore, state.points)
+        
+        }
+
+    default:
+      throw new Error('No opertion supported')
+  }
+}
+
+const App = () => {
+
+  const [{ questions, status ,currentQuestion , answer ,points,highScore}, dispatch] = useReducer(reducer, initialState)
+
+  const totalsPoints = questions.reduce((prev , curr) => prev +curr.points ,0)
+  const numQuestions = questions.length
+
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }))
+
+  }, [])
+
+  return (
+    <div className='app'>
+      <Header />
+      <main>
+        {status === "error" && <Error />}
+        {status === "loading" && <Loader />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && <>
+        <Progress currentQuestion={currentQuestion} numQuestions={numQuestions} points={points} totalsPoints={totalsPoints} answer={answer}
+        />
+        <Question question={questions[currentQuestion]} answer={answer} dispatch= {dispatch} currentQuestion={currentQuestion} numQuestions={numQuestions}/>
+        </> 
+        }
+
+        {status ==="finished" && <FinishScreen totalsPoints={totalsPoints} points={points} highScore={highScore}/>}
+      </main>
+    </div>
+  )
+}
+export default App
